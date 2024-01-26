a89dbb993dd5d6bbc0f559920f7d71dc4e6c18e2
itsguim <italoguimaraes13@hotmail.com>
2024-01-26 20:19:23 -0300

    :recycle:refactor: css modules & verificação ao inserir dados

--- src/js/main.js
+++ src/js/main.js
@@ -21,20 +21,24 @@ function approvedEmoji() {
 function insertScore() { // **** Insere os dados na tabela & executa a att. da média.
   const gradeFormat = inputGrade.value.replace(/,/g, '.'),
     formattedGrade = parseFloat(gradeFormat).toFixed(2) //** Formatado pra substituir vírgula, e limitar casas decimais.
-  const insertRow = `
-  <tr class="t_insert">
-    <td id="activity">${inputActivity.value}</td>
-    <td id="grade">${formattedGrade}</td>
-    <td id="approved">
-      <div class="approved_img">
-        ${approvedEmoji()}
-      </div>
-    </td>
-  </tr>`
-  scoreRow.insertAdjacentHTML('beforebegin', insertRow)
-  // ***** Calcula a média à cada inserção & Checa se está aprovado.
-  calcAverage()
-  isApproved()
+
+  if (!isNaN(formattedGrade) && formattedGrade >= 0 && formattedGrade <= 10) {
+    const insertRow = `
+    <tr class="t_insert">
+      <td id="activity">${inputActivity.value}</td>
+      <td id="grade">${formattedGrade}</td>
+      <td id="approved">
+        <div class="approved_img">
+          ${approvedEmoji()}
+        </div>
+      </td>
+    </tr>`
+    scoreRow.insertAdjacentHTML('beforebegin', insertRow)
+    // ***** Calcula a média à cada inserção & Checa se está aprovado.
+    calcAverage()
+    isApproved()
+    document.querySelector('form').reset()
+  }
 }
 
 function calcAverage() {
